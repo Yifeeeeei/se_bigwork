55e3723f41bcb74f4cd00163daddaef7347e1490
lyunchi19 <2605079332@qq.com>
2021-11-06 09:53:16 +0800

    Revert "aaa"
    This reverts commit b627b07dd9eebadc83a5c9689112c50d81b91162.

--- testv1.0/pages/index/index.js
+++ testv1.0/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello Wo',
+    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
