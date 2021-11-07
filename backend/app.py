b1634f6d87576669c5cc68eea63b15cbf62d67b6
if <yifeichen.chen@hotmail.com>
2021-11-07 18:10:48 +0800

    添加了api文档可以查看了，在stucture/docs里

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,196 @@
 from flask import Flask
 from flask import request
-from flask import render_template, redirect,url_for
+from flask import render_template, redirect, url_for
 import json
+from backend.structure import member
+import structure.functions as func
+from structure.member import Member
+from structure.club import Club
+from structure.container import Container
+from structure.ddl import DDL
+from structure.notice import Notice
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_word():
     return 'Hello World'
+# 获取
 
-@app.route('/api/get_container',methods = ['POST'])
-def api_get_container():
+
+@app.route('/api/get/container', methods=['POST'])
+def apiGetContainer():
     data = request.get_data()
     json_data = json.loads(data.decode("utf-8"))
-    container_id = json_data['container_id']
+    container_id = json_data['id']
+    container = func.DBgetContainer(container_id)
+    return container.toJson()
+
+
+@app.route('/api/get/club', methods=['POST'])
+def apiGetClub():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    club_id = json_data['id']
+    club = func.DBgetClub(club_id)
+    return club.toJson()
+
+
+@app.route('/api/get/member', methods=['POST'])
+def apiGetMember():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    member_id = json_data['id']
+    member = func.DBgetMember(member_id)
+    return member.toJson()
+
+
+@app.route('/api/get/ddl', methods=['POST'])
+def apiGetDDL():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    ddl_id = json_data['id']
+    ddl = func.DBgetDDL(ddl_id)
+    return ddl.toJson()
+
+
+@app.route('/api/get/notice', methods=['POST'])
+def apiGetNotice():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    notice_id = json_data['id']
+    notice = func.DBgetNotice(notice_id)
+    return notice.toJson()
+
+# 更新一些数据，需要发送完整的包！
+
+
+@app.route('/api/update/club', methods=['POST'])
+def apiUpdateClub():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    club = Club()
+    club.fromDic(json_data)
+    func.DBupdateClub(club)
+
+
+@app.route('/api/update/container', methods=['POST'])
+def apiUodateContainer():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    container = Container()
+    container.fromDic(json_data)
+    func.DBupdateContainer(container)
+
+
+@app.route('/api/update/ddl', methods=['POST'])
+def apiUpdateDDL():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    ddl = DDL()
+    ddl.fromDic(json_data)
+    func.DBupdateDDL(ddl)
+
+
+@app.route('/api/update/member', methods=['POST'])
+def apiUpdateMember():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    member = Member()
+    member.fromDic(json_data)
+    func.DBupdateMember(member)
+
+
+@app.route('/api/update/notice', methods=['POST'])
+def apiUpdateNotice():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    notice = Notice()
+    notice.fromDic(json_data)
+    func.DBupdateMember(notice)
+
+#create
+@app.route('/api/create/club', methods=['POST'])
+def apiCreateClub():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    club = Club()
+    club.fromDic(json_data)
+    club.generateRandomId()
+    func.DBnewClub(club)
+
+
+@app.route('/api/create/container', methods=['POST'])
+def apiCreateContainer():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    container = Container()
+    container.fromDic(json_data)
+    container.generateRandomId()
+    func.DBnewContainer(container)
+
+
+@app.route('/api/create/ddl', methods=['POST'])
+def apiCreateDDL():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    ddl = DDL()
+    ddl.fromDic(json_data)
+    ddl.generateRandomId()
+    func.DBnewDDL(ddl)
+
+
+@app.route('/api/create/member', methods=['POST'])
+def apiCreateMember():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    member = Member()
+    member.fromDic(json_data)
+    func.DBnewMember(member)
+
+
+@app.route('/api/create/notice', methods=['POST'])
+def apiCreateNotice():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    notice = Notice()
+    notice.fromDic(json_data)
+    notice.generateRandomId()
+    func.DBnewNotice(notice)
+
+
+#check
+@app.route('/api/check/ddl',methods=['POST']) 
+def apiCheckDDL():
+    #{ddl_id:xxx,checker_id:xxx}
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    func.checkDDL(json_data["ddl_id"],json_data["checker_id"])
+    
+@app.route('/api/check/notice',methods=['POST']) 
+def apiCheckNotice():
+    #{notice_id:xxx,checker_id:xxx}
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    func.checkNotice(json_data["notice_id"],json_data["checker_id"])
+    
+#modify
+@app.route('/api/modify/name',methods=['POST'])
+def apiModifyName():
+    #{member_id:xxx,new_name:xxx}
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    func.changeName(json_data['member_id'],json_data['new_name'])
+    
+#actions
+@app.route('/api/actions/join_container',methods=['POST'])
+def apiActionsJoinContainer():
+    #{member_id,container_id}
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    func.joinContainer(json_data['member_id'],json_data['container_id'])
+    
     
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=11451)
+    app.run(host='0.0.0.0', port=11451)
--- backend/structure/docs/api.md
+++ backend/structure/docs/api.md
@@ -1,5 +1,110 @@
-# Club (name:str, boss_member:Member) -> Club
-## createContainer (name:str, upper_container:Container)
-将会在自己旗下创建一个Container，并将它挂到upper_container上
+如果没有说明，method为post
+除了member类之外的id应该都会自动分配，就暂定member的id是微信号吧
+# 如果要求传入某个类json，按照接下来的json格式传入(xxx表示某个值)
+## Club类json
+{
+    id:xxx,
+    name:xxx,
+    discription:xxx,
+    containers_id:[xxx,xxx,xxx...], //列表元素为包含的container的id
+    root_container_id:xxx//根container也就是只包含社长的那一个container
+}
+## Container类json
+{
+    id:xxx,
+    name:xxx,
+    belongs_to_club_id:xxx,//该container隶属的Club的id
+    upper_container_id:xxx,//该container的上级container的id
+    contains:[xxx,xxx,xxx...]//列表，内部元素为包含member的id
+    lower_containers_id:[xxx,xxx,xxx...]//列表，内部元素为下层Container的id
+}
+## DDL类json
+{
+    id:xxx,
+    name:xxx,//标题
+    club_id:xxx,//填写社团id
+    post_date:xxx,//发送时间
+    end_date:xxx,//deadline
+    content:xxx,//内容
+    from_member_id:xxx//发出者id
+    to_members_id:[xxx,xxx,xxx...]//接收者member id列表
+    not_done_members_id:[xxx,xxx,xxx...]//没有完成的member id列表
+}
+## Member类json
+{
+    id:xxx,
+    name:xxx,
+    belongs_to_container_id = [xxx,xxx,xxx...],//这个人处于的container id列表
+    ddls_received_id = [xxx,xxx,xxx...],//接收到了ddl id列表
+    ddls_sent_id = [xxx,xxx,xxx...],//发送过的ddl id列表
+    ddls_checked_id = [xxx,xxx,xxx...],//这个人自己check过的ddl id列表
+    notices_received_id = [xxx,xxx,xxx...],//接收到的notice id列表
+    notices_checked_id = [xxx,xxx,xxx...],//这个人check过的notice id列表
+    notices_sent_id = [xxx,xxx,xxx...],//这个人发出的notice id列表
+}
+## Notice类json
 
-# Container (name:str,upper_container:Container or None,belongs_to_club:Club)
+{
+    id:xxx,
+    name:xxx,
+    club_id:xxx,//社团id
+    post_date:xxx,//发出日期
+    content:xxx,//内容
+    from_member_id:xxx,//发出member id
+    to_members_id:[xxx,xxx,xxx...]//接收者member id列表
+}
+
+# 获取
+## /api/get/container
+{id:xxx}->一个Container的json
+## /api/get/club
+{id:xxx}->一个Club的json
+## /api/get/member
+{id:xxx}->一个Member的json
+## /api/get/ddl
+{id:xxx}->一个DDL的json
+## /api/get/notice
+{id:xxx}->一个Notice的json
+
+# 更新数据
+用于更新一个比如个人信息之类的东西，就像各种列表什么的就别用这几个更改
+## /api/update/club
+传入一个Club的json，内含club_id对应的club的新信息
+## /api/update/container
+传入一个Container的json，内含container_id对应的container的新信息
+## /api/update/ddl
+类似上面
+## /api/update/member
+类似上面
+## /api/updata/notice
+类似上面
+
+# 创建新数据
+## /api/create/club
+传入Club json(id瞎填，会自动生成)，后台创建
+## /api/create/container
+传入container json(id瞎填，会自动生成)，后台创建
+会自动调整所在club和上级container
+## /api/create/ddl
+类似上面
+会自动为调整发出者和接受者相关项
+## /api/create/notice
+类似上面
+会自动调整发出则和接受着相关项
+## /api/create/member
+传入Member json，id填微信id（吧）
+
+# 打勾操作
+## api/check/ddl
+传入{ddl_id:xxx,checker_id:xxx}，为该条ddl打勾
+## /api/check/notice
+传入{notice_id:xxx,checker_id:xxx}，为该notice打勾
+
+# 更改信息
+## /api/modify/name
+传入{member_id:xxx,new_name:xxx}，为member改名
+
+# 操作
+## /api/actions/join_container
+传入{member_id,container_id}
+将一位一个member加入到一个container中
