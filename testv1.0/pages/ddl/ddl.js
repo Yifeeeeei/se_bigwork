5a638bad4a893f3b564671be59531e58e0d5d9fc
zuer001 <59675017+zuer001@users.noreply.github.com>
2021-11-16 20:57:05 +0800

    基本实现了ddl界面

--- testv1.0/pages/ddl/ddl.js
+++ testv1.0/pages/ddl/ddl.js
@@ -8,26 +8,104 @@ Page({
         ddlData:[
             {
                 string:"123",
-                outdate:true
+                outdate:0,
+                uid:12341
             },
             {
                 string:"456",
-                outdate:false
+                outdate:1,
+                uid:5421
             },
             {
                 string:"789",
-                outdate:false
+                outdate:2,
+                uid:58172
             }
-        ]
+        ],
+        menuTapCurrent:0,
+        informData:[
+          {
+              string:"123",
+              uid:12341
+          },
+          {
+              string:"456",
+              uid:5421
+          },
+          {
+              string:"789",
+              uid:58172
+          }
+      ],
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
     },
-
+    menuTap:function(e){
+      var current=e.currentTarget.dataset.current;//获取到绑定的数据
+      //改变menuTapCurrent的值为当前选中的menu所绑定的数据
+      this.setData({
+      menuTapCurrent:current
+      });
+       
+       
+    },
+    toinform:function(e){
+      let a=e.currentTarget.dataset.uid;
+      wx.navigateTo({
+        url: '../informdetail/informdetail?uid='+a,
+      })
+    },
+    todetail:function(e){
+        let a=e.currentTarget.dataset.uid;
+        if(e.currentTarget.dataset.flag==1)
+        {
+            wx.showModal({
+                title: '提示',
+                content: '内容:'+e.currentTarget.dataset.string+'\n'+'状态:未完成',
+                confirmText:'已完成',
+                success: function (res) {
+                  if (res.confirm) {
+                    console.log('确')
+                  } else {
+                    console.log('取消')
+                  }
+                }
+              })
+        }
+        else if(e.currentTarget.dataset.flag==0)
+        {
+            wx.showModal({
+                title: '提示',
+                content: '内容:'+e.currentTarget.dataset.string+'\n'+'状态:过期',
+                success: function (res) {
+                  if (res.confirm) {
+                    console.log('确')
+                  } else {
+                    console.log('取消')
+                  }
+                }
+              })
+        }
+        else
+        {
+            wx.showModal({
+                title: '提示',
+                content: '内容:'+e.currentTarget.dataset.string+'\n'+'状态:已完成',
+                success: function (res) {
+                  if (res.confirm) {
+                    console.log('确')
+                  } else {
+                    console.log('取消')
+                  }
+                }
+              })
+        }
+        
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
