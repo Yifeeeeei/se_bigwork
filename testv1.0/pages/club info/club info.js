a3cb60c352de96b2dec2aec25d9ffa04525c067b
FlinchShrimp <ydj19@mails.tsinghua.edu.cn>
2021-11-27 09:32:26 +0800

    社团详情（一般）

---
+++ testv1.0/pages/club info/club info.js
@@ -0,0 +1,85 @@
+// pages/user info/user info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    menuitems: [{
+        text: '社团名称',
+        url: '#',
+        icon: '/images/club/1.png',
+        info: ''
+      },
+      {
+        text: '社团结构',
+        url: '#',
+        icon: '/images/club/2.png',
+        info: ''
+      },
+      {
+        text: '我的职务',
+        url: '#',
+        icon: '/images/club/3.png',
+        info: ''
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
